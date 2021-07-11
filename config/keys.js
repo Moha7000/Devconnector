a1c4cc167228be4a73a0ce79b009c1761ad0b6d8
unknown <moha.hossam96@gmail.com>
2021-07-11 09:08:44 +0200

    a user model created, passport configurationn,register a user and login routes added in user.js in routes/api

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb+srv://mohanad:mohanad@devconnector.9ypw7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-
+    mongoURI: 'mongodb+srv://mohanad:mohanad@devconnector.9ypw7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    secretOrKey: 'secret'
 }
